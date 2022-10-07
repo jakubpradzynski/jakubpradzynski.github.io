5eaa2f2ec4c408414cf49458d6228daca9f2ee70
Jakub Prądzyński <jakubpradzynski@gmail.com>
2022-10-07 10:31:22 +0200

    #12 | Remove script

--- multiple_find_queries.js
+++
@@ -1,9 +0,0 @@
-db = connect( 'mongodb+srv://blog:4p8moFTYv2hWhpbf@blog.xd7fbif.mongodb.net/sample_training?readPreference=secondary' );
-
-function delay(time) {
-  return new Promise(resolve => setTimeout(resolve, time));
-}
-
-for (let i = 0; i < 1_000; i++) {
-  delay(500).then(() => db.posts.findOne({}));
-}
